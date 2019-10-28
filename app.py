fa7e7087d339cd34da2d8b8c0e6bdc8f57d8bffe
9abinash <9abinash@gmail.com>
2019-10-28 23:53:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from keras.models import load_model
 import io
 from keras.applications import ResNet50
 from keras.preprocessing.image import img_to_array
@@ -6,138 +9,63 @@ from PIL import Image
 import numpy as np
 import flask
 from flask import request
+import warnings
+warnings.filterwarnings("ignore")
+import keras
 
-app = flask.Flask(__name__)
-#model = None
-
-
-#def load_model():
-#    """Load ResNet50 Keras model."""
-#    global model
-#    model = ResNet50(weights="imagenet")
+import pickle
 
 
-#load_model()
-from keras.models import load_model
-model = load_model('model.h5')
+app = Flask(__name__)
+# model = pickle.load(open('mobile_abi.h5', 'rb'))
 
+model=load_model('mobile_abi.h5')
 
-def prepare_image(image, target):
-    """Pre-process image to be ran with the model.
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    Parameters
-    ----------
-    image : BytesIO
-        Bytes object from user uploaded from interface
-    target : tuple
-        Image size to resize (244,244)
+# @app.route('/predict',methods=['POST'])
+# def predict():
+    # '''
+    # For rendering results on HTML GUI
+    # '''
+    # int_features = [int(x) for x in request.form.values()]
+    # final_features = [np.array(int_features)]
+    # prediction = model.predict(final_features)
 
-    Returns
-    -------
-    image
-        Ready to ingest into keras model
-    """
-    # if the image mode is not RGB, convert it
+    # output = round(prediction[0], 2)
+
+    # return render_template('index.html', prediction_text='Employee Salary should be  ')
+
+def preprocess_image(image, target_size):
     if image.mode != "RGB":
         image = image.convert("RGB")
-
-    # resize the input image and preprocess it
-    image = image.resize(target)
+    image = image.resize(target_size)
     image = img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-    image = imagenet_utils.preprocess_input(image)
+    img_array_expanded_dims = np.expand_dims(image, axis=0)
 
-    # return the processed image
-    return image
+    return keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
 
+# def prepare_image(file):
+    # img_path = '/Users/abigiri/Desktop/'
+    # img = image.load_img(img_path + file, target_size=(224, 224))
+    # img_array = image.img_to_array(img)
+    # img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    # return keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
 
+output=''
 @app.route("/predict", methods=["POST"])
 def predict():
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {"success": False}
-
-    # ensure an image was properly uploaded to our endpoint
-    if flask.request.method == "POST":
-        if flask.request.files.get("image"):
-            # read the image in PIL format
-            image = flask.request.files["image"].read()
-            image = Image.open(io.BytesIO(image))
-
-            # preprocess the image and prepare it for classification
-            image = prepare_image(image, target=(224, 224))
-
-            # classify the input image and then initialize the list
-            # of predictions to return to the client
-            preds = model.predict(image)
-            results = imagenet_utils.decode_predictions(preds)
-            data["predictions"] = []
-
-            # loop over the results and add them to the list of
-            # returned predictions
-            for (imagenetID, label, prob) in results[0]:
-                r = {"label": label, "probability": float(prob)}
-                data["predictions"].append(r)
-
-            # indicate that the request was a success
-            data["success"] = True
-
-    # return the data dictionary as a JSON response
-    return flask.jsonify(data)
-
-
-@app.route("/", methods=["POST", "GET"])
-def home():
-    # initialize the data dictionary that will be returned from the
-    # view
-    data = {"success": False}
-
-    if request.method == "GET":
-        return """
-        <!doctype html>
-        <title>Image Classification</title>
-        <h1>Image Classification</h1>
-        <h3>ResNet50 trained on ImageNet</h3>
-        <strong>Note:</strong> output will be in JSON
-        <form method=post enctype=multipart/form-data>
-        <p><input type=file name=file>
-            <input type=submit value=Select Prediction Image>
-        </form>
-        """
-
-    # ensure an image was properly uploaded to our endpoint
-    if flask.request.method == "POST":
-        # read the image in PIL format
-        image = request.files["file"].read()
-        image = Image.open(io.BytesIO(image))
-
-        # preprocess the image and prepare it for classification
-        image = prepare_image(image, target=(224, 224))
-        # classify the input image and then initialize the list
-        # of predictions to return to the client
-        preds = model.predict(image)
-        results = imagenet_utils.decode_predictions(preds)
-        data["predictions"] = []
-
-        # loop over the results and add them to the list of
-        # returned predictions
-        for (imagenetID, label, prob) in results[0]:
-            r = {"label": label, "probability": float(prob)}
-            data["predictions"].append(r)
-
-        # indicate that the request was a success
-        data["success"] = True
-
-    # return the data dictionary as a JSON response
-    return flask.jsonify(data)
-
-
+    image = flask.request.files.get('imagefile', '')
+    image = image.read()
+    image = Image.open(io.BytesIO(image))
+    processed_image = preprocess_image(image, target_size=(224, 224))
+    prediction = model.predict(processed_image)
+    if prediction[0][0] > prediction[0][1]:
+        text='Congrats!! you are a cat person.'
+    else:
+        text='Congrats!! you are a dog person. Kevin the dog likes you <3'
+    return render_template('index.html', prediction_text=text )
 if __name__ == "__main__":
-    print(
-        (
-            "* Loading Keras model and Flask starting server..."
-            "please wait until server has fully started"
-        )
-    )
-    load_model()
-    app.run()
+    app.run(debug=True)
